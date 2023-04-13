5498e4ec93ae64792403030b636e4c4a50fbe4c1
NeroHin <hin1997101@gmail.com>
2023-04-13 06:31:03 +0000

    fix: modify gpu num

--- ptuning/ds_train_finetune.sh
+++ ptuning/ds_train_finetune.sh
@@ -3,7 +3,7 @@ LR=1e-4
 
 MASTER_PORT=$(shuf -n 1 -i 10000-65535)
 
-deepspeed --num_gpus=4 --master_port $MASTER_PORT main.py \
+deepspeed --num_gpus=2 --master_port $MASTER_PORT main.py \
     --deepspeed deepspeed.json \
     --do_train \
     --train_file data/train.json \
